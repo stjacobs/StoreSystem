766d0c19e412bfb06e156725a9b85ab69414733e
stjacobs <carlmikaelalmgren@gmail.com>
2014-10-24 22:11:05 +0200

    scanner

--- StoreSystem/src/StoreSystem.java
+++ StoreSystem/src/StoreSystem.java
@@ -1,8 +1,11 @@
+import java.util.Scanner;
+
 
 public class StoreSystem {
 	
 	public static void main(String[] args){
 		System.out.println("Hej på dej");
+		Scanner in = new Scanner(System.in);
 	}
 	
 }
